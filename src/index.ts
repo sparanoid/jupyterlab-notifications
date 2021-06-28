36b92479e6fd6eb8a1319af43dae51a2b5c81a8d
Mariko Wakabayashi <mariko@mwkby.com>
2021-06-27 21:37:48 -0400

    Notify for failed notebook cell executions

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,12 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-import { NotebookActions } from '@jupyterlab/notebook';
-import { IObservableJSON } from '@jupyterlab/observables';
+import { KernelError, Notebook, NotebookActions } from '@jupyterlab/notebook';
+import { Cell } from '@jupyterlab/cells';
 import { ISettingRegistry } from '@jupyterlab/settingregistry';
 
 import { checkBrowserNotificationSettings } from './settings';
 
-/**
- * Extracts Code Cell Start and End Time
- */
-function extractExecutionMetadata(metadata: IObservableJSON): [Date, Date] {
-  const executionMetadata = Object.assign({}, metadata.get('execution') as any);
-  const cellStartTime = new Date(
-    executionMetadata['shell.execute_reply.started'] ||
-      executionMetadata['iopub.execute_input']
-  );
-  const cellEndTime = new Date(executionMetadata['shell.execute_reply']);
-  return [cellStartTime, cellEndTime];
-}
 
 /**
  * Constructs notification message and displays it.
@@ -29,14 +17,20 @@ function displayNotification(
   cellNumber: number,
   notebookName: string,
   reportCellNumber: boolean,
-  reportCellExecutionTime: boolean
+  reportCellExecutionTime: boolean,
+  failedExecution: boolean,
+  error: KernelError | null
 ): void {
   const notificationPayload = {
     icon: '/static/favicon.ico',
     body: ''
   };
+  const title = failedExecution ? `${notebookName} Failed!` : `${notebookName} Completed!`;
   let message = '';
-  if (reportCellNumber && reportCellExecutionTime) {
+
+  if (failedExecution) {
+    message = error ? `${error.errorName} ${error.errorValue}` : '';
+  } else if (reportCellNumber && reportCellExecutionTime) {
     message = `Cell[${cellNumber}] Duration: ${cellDuration}`;
   } else if (reportCellNumber) {
     message = `Cell Number: ${cellNumber}`;
@@ -45,19 +39,56 @@ function displayNotification(
   }
 
   notificationPayload.body = message;
-  new Notification(`${notebookName} Cell Completed!`, notificationPayload);
+  new Notification(title, notificationPayload);
+}
+
+/**
+ * Trigger notification.
+ */
+function triggerNotification(
+  cell: Cell,
+  notebook: Notebook,
+  cellStartTime: Date,
+  cellEndTime: Date,
+  minimumCellExecutionTime: number,
+  reportCellNumber: boolean,
+  reportCellExecutionTime: boolean,
+  failedExecution: boolean,
+  error: KernelError | null
+) {
+  const codeCell = cell.model.type === 'code';
+  const nonEmptyCell = cell.model.value.text.length > 0;
+
+  if (codeCell && nonEmptyCell) {
+    const diff = new Date(<any>cellEndTime - <any>cellStartTime);
+    if (diff.getSeconds() >= minimumCellExecutionTime) {
+      const cellDuration = diff.toISOString().substr(11, 8);
+      const cellNumber = notebook.activeCellIndex;
+      const notebookName = notebook.title.label;
+      displayNotification(
+        cellDuration,
+        cellNumber,
+        notebookName,
+        reportCellNumber,
+        reportCellExecutionTime,
+        failedExecution, 
+        error
+      );
+    }
+  }
 }
 
 const extension: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-notifications:plugin',
   autoStart: true,
-  optional: [ISettingRegistry],
+  requires: [ISettingRegistry],
   activate: async (app: JupyterFrontEnd, settingRegistry: ISettingRegistry) => {
     checkBrowserNotificationSettings();
     let enabled = true;
     let minimumCellExecutionTime = 60;
     let reportCellExecutionTime = true;
     let reportCellNumber = true;
+
     if (settingRegistry) {
       const setting = await settingRegistry.load(extension.id);
       const updateSettings = (): void => {
@@ -68,42 +99,33 @@ const extension: JupyterFrontEndPlugin<void> = {
           .composite as boolean;
         reportCellNumber = setting.get('report_cell_number')
           .composite as boolean;
+        
       };
       updateSettings();
       setting.changed.connect(updateSettings);
     }
 
+    let cellStartTime = new Date();
+
+    NotebookActions.executionScheduled.connect((_, args) => {
+      if (enabled) {
+        cellStartTime = new Date();
+      }
+    });
+
     NotebookActions.executed.connect((_, args) => {
       if (enabled) {
-        const { cell, notebook } = args;
-        const codeCell = cell.model.type === 'code';
-        const nonEmptyCell = cell.model.value.text.length > 0;
-        const metadata = cell.model.metadata;
-        if (codeCell && nonEmptyCell) {
-          if (metadata.has('execution')) {
-            const [cellStartTime, cellEndTime] = extractExecutionMetadata(
-              metadata
-            );
-            const diff = new Date(<any>cellEndTime - <any>cellStartTime);
-            if (diff.getSeconds() >= minimumCellExecutionTime) {
-              const cellDuration = diff.toISOString().substr(11, 8);
-              const cellNumber = notebook.activeCellIndex;
-              const notebookName = notebook.title.label;
-              displayNotification(
-                cellDuration,
-                cellNumber,
-                notebookName,
-                reportCellNumber,
-                reportCellExecutionTime
-              );
-            }
-          } else {
-            alert(
-              'Notebook Cell Timing needs to be enabled for Jupyterlab Notifications to work. ' +
-                'Please go to Settings -> Advanced Settings Editor -> Notebook and update setting to {"recordTiming": true}'
-            );
-          }
-        }
+        const { cell, notebook, success, error } = args;
+        const cellEndTime = new Date();
+        triggerNotification(cell, 
+                            notebook, 
+                            cellStartTime,
+                            cellEndTime,
+                            minimumCellExecutionTime, 
+                            reportCellNumber, 
+                            reportCellExecutionTime,
+                            !success,
+                            error);
       }
     });
   }
